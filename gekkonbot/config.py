8d12d780de7664e3bcd636e7623147f861c9bb3d
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2018-10-14 20:44:18 +0300

    Config encoding fix

--- gekkonbot/config.py
+++ gekkonbot/config.py
@@ -3,7 +3,7 @@ import json
 
 CONFIG_DIR = os.getcwd()
 
-with open(os.path.join(CONFIG_DIR, 'config.json')) as config_file:
+with open(os.path.join(CONFIG_DIR, 'config.json'), 'r', encoding='utf-8') as config_file:
     config = json.loads(config_file.read())
 
 config['google-credentials-path'] = os.path.join(CONFIG_DIR, 'google_service.json')
