297cef91419d2e560bff139cbd2d6079b281258e
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2019-12-02 12:08:41 +0300

    Save item code as integer

--- gekkonbot/spreadsheets.py
+++ gekkonbot/spreadsheets.py
@@ -152,7 +152,7 @@ class OrderList(SpreadsheetService):
         self.last_id += 1
         order_id = "{:05}".format(self.last_id)
         body = {
-            'values': [[order_id, str(item[0]), item[1], count, customer, time, '', '', '', '', deadline, comment]]
+            'values': [[order_id, item[0], item[1], count, customer, time, '', '', '', '', deadline, comment]]
         }
         query = self.service.spreadsheets().values().append(spreadsheetId=self.spreadsheet_id, range=self.range,
                                                             body=body, valueInputOption="RAW")
